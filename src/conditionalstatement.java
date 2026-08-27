b206ec788c4cbec644cc9cf99a1d79710341979d
Chandan Kumar Gupta <chandankone2005@gmail.com>
2026-08-28 01:57:44 +0530

    Day 1 - Java Core Basics

---
+++ src/conditionalstatement.java
@@ -0,0 +1,15 @@
+ import java.util.*;
+public class conditionalstatement {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int marks = sc.nextInt();
+        if(marks>=70) {
+            System.out.println("pass");
+        }
+            else {
+                System.out.println("fail");
+            }
+        }
+    }
+
