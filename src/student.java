d7d8ccf56255bc64c245e409fae1a1ca06ae7015
Chandan Kumar Gupta <chandankone2005@gmail.com>
2026-08-28 02:32:48 +0530

    Day 2 - Java oops basic

---
+++ src/student.java
@@ -0,0 +1,18 @@
+ class abes{
+    int age;
+    String name;
+    int marks;
+
+}
+public class student{
+    static void main(String[] args) {
+        abes s1 = new abes();
+        s1.name="chandan";
+        s1.age=21;
+        s1.marks=90;
+        System.out.println(s1.name);
+        System.out.println(s1.age);
+        System.out.println(s1.marks);
+    }
+
+}
