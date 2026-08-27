b4aff722747192ff2ec3276d585df56a89ff0fcc
Chandan Kumar Gupta <chandankone2005@gmail.com>
2026-08-28 01:47:12 +0530

    Day 1 - Java Core Basics

---
+++ src/InputfromUser.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class InputfromUser {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+    System.out.println("my name is " + name);
+    }
+}
---
+++ src/usernameInput.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class usernameInput {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println("my name is " + name);
+    }
+}
+
